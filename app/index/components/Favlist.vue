4736ee88f3e8bc16c56a65d2b399aea16fdc17f4
靳龙 <394268118@qq.com>
2017-02-07 18:06:38 +0800

    webpackProj init

---
+++ app/index/components/Favlist.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="n in 10">div</div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        msg:"hello world"
+      }
+    }
+  }
+</script>
+
+<style>
+  html{
+    background:red;
+  }
+<style>
